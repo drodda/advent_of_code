529c9b8d2f4864ae3597c13fab6f227d8778e067
David <dave.rodda@gmail.com>
2020-12-23 08:23:01 +1100

    Challenge 22: Tidy, fixed a logic problem that did not affect the result

--- challenge_22.py
+++ challenge_22.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 import traceback
-import collections
 import copy
 
 from utils import *
-
-import ipdb
 
 
 ###############################################################################
@@ -20,7 +17,8 @@ def play_game(cards1_in, cards2_in):
     # Loop
     while cards1 and cards2:
         play_round(cards1, cards2)
-    return cards1, cards2
+    player_1_win = len(cards1) > len(cards2)
+    return cards1, cards2, player_1_win
 
 
 def play_round(cards1, cards2):
@@ -38,13 +36,22 @@ def play_regular(card1, card2, cards1, cards2):
 
 
 def allocate_cards(card1, card2, cards1, cards2, player_1_win):
-    """ Allocate cards to player 1 if player_1_winner, else to player 2"""
+    """ Allocate cards to player 1 if player_1_win, else to player 2"""
     if player_1_win:
         cards1.append(card1)
         cards1.append(card2)
     else:
         cards2.append(card2)
         cards2.append(card1)
+
+
+def print_result(cards1, cards2, player_1_win):
+    winner = "1" if player_1_win else "2"
+    print_debug(f"Player 1: {cards1}")
+    print_debug(f"Player 2: {cards2}")
+    print(f"Winner: {winner}")
+    print(f"Player 1: {calculate_score(cards1)}")
+    print(f"Player 2: {calculate_score(cards2)}")
 
 
 def calculate_score(cards):
@@ -65,17 +72,10 @@ class GameOver(Exception):
         self.cards1 = cards1
         self.cards2 = cards2
 
-    @property
-    def winner(self):
-        return 1 if self.player_1_win else 2
 
-    def __str__(self):
-        return f"Winner: Player {self.winner}"
-
-
-def play_game_recursive(cards1_in, cards2_in, raise_on_exit=False, g=1):
+def play_game_recursive(cards1_in, cards2_in, g=1):
     """ Play a whole game using recursive rules
-        Returns cards on completion, unless raise_on_exit, in which case GameOver will be raised
+        Returns cards on completion and a boolean indicating if player 1 wins
     """
     # Copy cards
     cards1 = copy.copy(cards1_in)
@@ -85,19 +85,19 @@ def play_game_recursive(cards1_in, cards2_in, raise_on_exit=False, g=1):
     history2 = []
     r = 1
     try:
+        # Loop
         while cards1 and cards2:
             play_round_recursive(cards1, cards2, history1, history2, g, r)
             r += 1
-    except GameOver:
-        pass
-    if raise_on_exit:
-        raise GameOver(len(cards1) > 0, cards1, cards2)
-    return cards1, cards2
+        player_1_win = len(cards1) > len(cards2)
+    except GameOver as result:
+        player_1_win = result.player_1_win
+    return cards1, cards2, player_1_win
 
 
 def play_round_recursive(cards1, cards2, history1, history2, g=1, r=1):
     """ Play a single round of a recursive game. Cards are modified in place
-        Raises GameOver if this is an end of game
+        Raises GameOver if this is an early end of game
     """
     print_debug(f"Game {g} Round {r}: {cards1} vs {cards2}")
     if cards1 in history1 or cards2 in history2:
@@ -111,13 +111,13 @@ def play_round_recursive(cards1, cards2, history1, history2, g=1, r=1):
         _cards1 = cards1[:card1]
         _cards2 = cards2[:card2]
         # Play the game
-        try:
-            print_verbose(f"Playing a game {g+1} for cards {card1} {cards1}, {card2} {cards2}")
-            play_game_recursive(_cards1, _cards2, True, g+1)
-        except GameOver as result:
-            print_verbose(f"Winner of game {g+1} for cards {card1} {cards1}, {card2} {cards2}: {result.winner}")
-            allocate_cards(card1, card2, cards1, cards2, result.player_1_win)
+        print_verbose(f"Playing a game {g+1} for cards {card1} {cards1}, {card2} {cards2}")
+        _, _, player_1_win = play_game_recursive(_cards1, _cards2, g+1)
+        winner = "1" if player_1_win else "2"
+        print_verbose(f"Winner of game {g+1} for cards {card1} {cards1}, {card2} {cards2}: {winner}")
+        allocate_cards(card1, card2, cards1, cards2, player_1_win)
     else:
+        # Not recursive round: play a regular round
         play_regular(card1, card2, cards1, cards2)
 
 
@@ -127,23 +127,17 @@ def play_round_recursive(cards1, cards2, history1, history2, g=1, r=1):
 def main():
     args = parse_args()
     data_file = data_file_path_main(test=args.test)
-    (_, *cards1_str), (_, *cards2_str) = read_multilines(data_file)
-    cards1 = list(map(int, cards1_str))
-    cards2 = list(map(int, cards2_str))
+    cards1_str, cards2_str = read_multilines(data_file)
+    cards1 = list(map(int, cards1_str[1:]))
+    cards2 = list(map(int, cards2_str[1:]))
 
     print("Part 1")
-    _cards1, _cards2 = play_game(cards1, cards2)
-    print_debug(f"Player 1: {_cards1}")
-    print_debug(f"Player 2: {_cards2}")
-    print(f"Player 1: {calculate_score(_cards1)}")
-    print(f"Player 2: {calculate_score(_cards2)}")
+    _cards1, _cards2, _player_1_win = play_game(cards1, cards2)
+    print_result(_cards1, _cards2, _player_1_win)
 
     print("Part 2")
-    _cards1, _cards2 = play_game_recursive(cards1, cards2)
-    print_debug(f"Player 1: {_cards1}")
-    print_debug(f"Player 2: {_cards2}")
-    print(f"Player 1: {calculate_score(_cards1)}")
-    print(f"Player 2: {calculate_score(_cards2)}")
+    _cards1, _cards2, _player_1_win = play_game_recursive(cards1, cards2)
+    print_result(_cards1, _cards2, _player_1_win)
 
 
 if __name__ == "__main__":
