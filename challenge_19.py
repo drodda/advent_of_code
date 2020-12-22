11a200f7868484a815b1245b20b4cf059d2ffc34
David <dave.rodda@gmail.com>
2020-12-22 19:53:10 +1100

    Completed Challenge 19 Part 2: complete rewrite

--- challenge_19.py
+++ challenge_19.py
@@ -6,104 +6,67 @@ import collections
 from utils import *
 
 
-class InvalidRule(Exception):
-    pass
+RECURSIVE_RULES = [
+    "8: 42 | 42 8",
+    "11: 42 31 | 42 11 31"
+]
 
 
-class RuleCheckFailed(Exception):
-    pass
-
-
-class Rule:
-    def __init__(self, rule_text):
-        self.condition = self.parse_rule(rule_text)
-
-    def parse_rule(self, rule_text):
-        raise NotImplemented
-
-    def validate_partial(self, rules, text):
-        """ Check text against rule
-            Returns the remainder of text after validating rule if valid, or raises RuleCheckFailed if invalid
-        """
-        raise NotImplemented
-
-    def validate(self, rules, text):
-        try:
-            return self.validate_partial(rules, text) == ""
-        except RuleCheckFailed:
+def test_message(rules, message, rule_list):
+    """ Test if message can be produced using rule_list """
+    if not message and not rule_list:
+        return True
+    if not message or not rule_list:
+        return False
+    rule = rules[rule_list[0]]
+    if isinstance(rule, str):
+        # Literal: check
+        if message[0] != rule:
             return False
-
-    def __repr__(self):
-        return f"<{self.condition}>"
-
-
-class LiteralRule(Rule):
-    def parse_rule(self, rule_text):
-        result = rule_text.strip("\"")
-        return result
-
-    def validate_partial(self, rules, text):
-        if not text.startswith(self.condition):
-            raise RuleCheckFailed(text)
-        return text[len(self.condition):]
-
-
-class CombinationRule(Rule):
-    def parse_rule(self, rule_text):
-        result = []
-        for combo in rule_text.split(" | "):
-            result.append(list(map(int, combo.split())))
-        return result
-
-    def validate_partial(self, rules, text):
-        for condition in self.condition:
-            try:
-                _text = text
-                for rule_num in condition:
-                    # if rule_num not in self._rules:
-                    #     raise InvalidRule
-                    rule = rules[rule_num]
-                    _text = rule.validate_partial(rules, _text)
-                return _text
-            except RuleCheckFailed:
-                pass
-        raise RuleCheckFailed
+        return test_message(rules, message[1:], rule_list[1:])
+    else:
+        # List of rules
+        for _rule_list in rule:
+            if test_message(rules, message, _rule_list + rule_list[1:]):
+                return True
+        return False
 
 
 def parse_rules(lines):
     rules = {}
-
     for line in lines:
         n_str, rule_text = line.split(": ")
         n = int(n_str)
         if rule_text.startswith("\"") and rule_text.endswith("\""):
-            rule = LiteralRule(rule_text)
+            rule = rule_text.strip("\"")
         else:
-            rule = CombinationRule(rule_text)
+            rule = [list(map(int, combo.split())) for combo in rule_text.split(" | ")]
         rules[n] = rule
     return rules
 
 
-###############################################################################
+def test_rules(rules, lines):
+    count = 0
+    for line in lines:
+        if test_message(rules, line, [0]):
+            count += 1
+    return count
+
 
 
 def main():
     args = parse_args()
     data_file = data_file_path_main(test=args.test)
+    # data_file = "data/challenge_19b_test.txt"
     lines_rules, lines_test = read_multilines(data_file)
 
-    # Build rules
-    rules = parse_rules(lines_rules)
-    rule = rules[0]
     print("Part 1")
-    valid_count = 0
-    for line in lines_test:
-        valid = rule.validate(rules, line)
-        print_debug(f"{line}: {valid}")
-        valid_count += int(valid)
-    print(valid_count)
+    rules = parse_rules(lines_rules)
+    print(test_rules(rules, lines_test))
 
     print("Part 2")
+    rules = parse_rules(lines_rules + RECURSIVE_RULES)
+    print(test_rules(rules, lines_test))
 
 
 if __name__ == "__main__":
