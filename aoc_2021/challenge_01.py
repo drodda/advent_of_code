839d51321f336d4e33d499f177b80da45d15291b
David <dave.rodda@gmail.com>
2021-12-02 07:43:30 +1100

    2021 Challenge 1

---
+++ aoc_2021/challenge_01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import traceback
+import numpy as np
+
+from utils import *
+
+
+def array_diff(array, n=1):
+    # Calculate the difference between elements of arr that are n apart
+    return array[n:] - array[:-n]
+
+
+def main():
+    args = parse_args()
+    data = np.array(list(read_list_int(data_file_path_main(test=args.test))))
+
+    log_always("Part 1:")
+    result_1 = np.sum(array_diff(data) > 0)
+    log_always(result_1)
+    log_always("Part 2:")
+    result_2 = np.sum(array_diff(data, 3) > 0)
+    log_always(result_2)
+
+
+if __name__ == "__main__":
+    # noinspection PyBroadException
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Killed")
+    except Exception:
+        traceback.print_exc()
+        sys.exit(-1)
