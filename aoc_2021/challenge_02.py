150cb7ee43a406d233ab9730a870bf16e1701ab7
David <dave.rodda@gmail.com>
2021-12-02 17:14:43 +1100

    2021 Challenge 2

---
+++ aoc_2021/challenge_02.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import traceback
+
+from utils import *
+
+
+def calculate_part_1(data):
+    dist = 0
+    depth = 0
+    for direction, value in data:
+        if direction == "forward":
+            dist += value
+        elif direction == "down":
+            depth += value
+        elif direction == "up":
+            depth -= value
+    log_info(f"Distance: {dist}, Depth: {depth}")
+    return dist * depth
+
+
+def calculate_part_2(data):
+    dist = 0
+    depth = 0
+    aim = 0
+    for direction, value in data:
+        if direction == "forward":
+            dist += value
+            depth += aim * value
+        elif direction == "down":
+            aim += value
+        elif direction == "up":
+            aim -= value
+    log_info(f"Distance: {dist}, Depth: {depth}")
+    return dist * depth
+
+
+def main():
+    args = parse_args()
+    lines = read_lines(data_file_path_main(test=args.test))
+    data = [line.split() for line in lines]
+    data = [(line[0], int(line[1])) for line in data]
+    log_always("Part 1:")
+    log_always(calculate_part_1(data))
+    log_always("Part 2:")
+    log_always(calculate_part_2(data))
+
+
+if __name__ == "__main__":
+    # noinspection PyBroadException
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Killed")
+    except Exception:
+        traceback.print_exc()
+        sys.exit(-1)
