842944ec0077353c80f1820a949e72a3537ff85f
David <dave.rodda@gmail.com>
2021-12-07 07:49:03 +1100

    2021 Challenge 6: rewrite for efficiency

--- aoc_2021/challenge_06.py
+++ aoc_2021/challenge_06.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import traceback
-import numpy as np
+from collections import Counter
 
 from utils import *
 
@@ -12,29 +12,35 @@ SPAWN_FIRST = 9
 SPAWN_NEXT = 7
 
 
-def calculate_part_1(data, n_days):
-    # Calculate the number of fish spawned (self + children) by a fish spawned for each day, in reverse
-    # Count up to -SPAWN_FIRST days, as existing fish were spawned in the past
-    fish_spawned_per_day = {}
-    for i in range(n_days, -SPAWN_FIRST, -1):
-        count = 1
-        for j in range(i + SPAWN_FIRST, n_days, SPAWN_NEXT):
-            count += fish_spawned_per_day[j]
-        fish_spawned_per_day[i] = count
-    # Sum the fish spawned from each fish in input
-    result = 0
-    for v in data:
-        result += fish_spawned_per_day[v - SPAWN_FIRST]
-    return result
+def parse_data(data, n):
+    """ Parse input data, return a list of counts of fish on each day of their spawn cycle """
+    counter = Counter(data)
+    fish = [counter.get(i, 0) for i in range(n)]
+    return fish
+
+
+def run_sim(fish_count, n_days):
+    """ Run simulation for n_days, return state """
+    assert(len(fish_count) == SPAWN_FIRST)
+    for i in range(n_days):
+        new = fish_count[0]
+        # Fish on day 0 spawn again in SPAWN_NEXT days
+        fish_count[SPAWN_NEXT] += new
+        # Each fish is 1 day closer to spawning; Spawn new fish from fish on day 0
+        fish_count = fish_count[1:] + [new]
+    return fish_count
 
 
 def main():
     args = parse_args()
-    data = np.array(list(map(int, open(data_file_path_main(test=args.test)).read().strip().split(","))))
+    data = map(int, open(data_file_path_main(test=args.test)).read().strip().split(","))
+    fish_count = parse_data(data, SPAWN_FIRST)
     log_always("Part 1:")
-    log_always(calculate_part_1(data, 80))
+    fish_count = run_sim(fish_count, 80)
+    log_always(sum(fish_count))
     log_always("Part 2:")
-    log_always(calculate_part_1(data, 256))
+    fish_count = run_sim(fish_count, 256 - 80)
+    log_always(sum(fish_count))
 
 
 if __name__ == "__main__":
