0f398aeb984c8d93d73bd863dedf94da6ce32b26
David <dave.rodda@gmail.com>
2021-12-09 15:49:48 +1100

    2021 Challenge 8: Readability improvements

--- aoc_2021/challenge_08.py
+++ aoc_2021/challenge_08.py
@@ -47,6 +47,9 @@ def lists_pop_by_condition(lst1, lst2, condition):
 
 
 def solve(input_values, output_values):
+    # Sort signals
+    input_values = [str_sort(item) for item in input_values]
+    output_values = [str_sort(item) for item in output_values]
     # Group input_values by length
     input_values_by_len = defaultdict(list)
     for item in input_values:
@@ -61,38 +64,30 @@ def solve(input_values, output_values):
     ]):
         raise SolutionError("Incorrect lengths of input values")
 
+    numbers = {}  # Store signals for each number identified
+
     # Identify easy values
-    val_1 = input_values_by_len[2].pop()
-    val_7 = input_values_by_len[3].pop()
-    val_4 = input_values_by_len[4].pop()
-    val_8 = input_values_by_len[7].pop()
+    numbers[1] = input_values_by_len[2].pop()
+    numbers[7] = input_values_by_len[3].pop()
+    numbers[4] = input_values_by_len[4].pop()
+    numbers[8] = input_values_by_len[7].pop()
     # 3: superset of 7
-    val_3 = list_pop_by_condition(input_values_by_len[5], lambda x: set(val_7).issubset(set(x)))
+    numbers[3] = list_pop_by_condition(input_values_by_len[5], lambda x: set(numbers[7]).issubset(set(x)))
     # 6: NOT superset of 7
-    val_6 = list_pop_by_condition(input_values_by_len[6], lambda x: not set(val_7).issubset(set(x)))
+    numbers[6] = list_pop_by_condition(input_values_by_len[6], lambda x: not set(numbers[7]).issubset(set(x)))
     # 5 is a subset of 9
-    val_5, val_9 = lists_pop_by_condition(input_values_by_len[5], input_values_by_len[6], lambda item_5, item_6: set(item_5).issubset(set(item_6)))
+    numbers[5], numbers[9] = lists_pop_by_condition(input_values_by_len[5], input_values_by_len[6], lambda item_5, item_6: set(item_5).issubset(set(item_6)))
     # Remaining items are now known
-    val_2 = input_values_by_len[5].pop()
-    val_0 = input_values_by_len[6].pop()
+    numbers[2] = input_values_by_len[5].pop()
+    numbers[0] = input_values_by_len[6].pop()
 
     # Create lookup to convert signals to numbers. Signals need to be sorted
-    values = {
-        str_sort(val_0): 0,
-        str_sort(val_1): 1,
-        str_sort(val_2): 2,
-        str_sort(val_3): 3,
-        str_sort(val_4): 4,
-        str_sort(val_5): 5,
-        str_sort(val_6): 6,
-        str_sort(val_7): 7,
-        str_sort(val_8): 8,
-        str_sort(val_9): 9,
-    }
+    signals = {signal: num for num, signal in numbers.items()}
 
+    # Identify output values from their signals
     result = 0
     for item in output_values:
-        result = result * 10 + values[str_sort(item)]
+        result = result * 10 + signals[item]
     return result
 
 
