3ac377a452edfae57e624300963f4e0fa7b307a3
David <dave.rodda@gmail.com>
2022-10-09 14:42:17 +1100

    2019 Challenge 23 improved logic

--- aoc_2019/challenge_23.py
+++ aoc_2019/challenge_23.py
@@ -14,31 +14,36 @@ from intcode_vm import *
 N_VMS = 50
 
 
+IDLE_SENTINEL = "is idle"
+
+
 class NicVM(VMThread):
     def __init__(self, mem, vm_id, router):
         super().__init__(mem)
-        self.stopped = False
         self.vm_id = vm_id
         self.router = router
-        # Synchronisation to ensure writes to a VM's input queue are atomic
-        self.input_lock = threading.Lock()
-        # replace output queue
+        # Replace output queue
         self.output = []
         self.is_idle = False
 
-    def stop(self):
-        self.stopped = True
-
     def step(self):
-        if self.stopped:
+        if self.router.stopped:
             raise StopIteration
         super().step()
+
+    def send_packet(self, x, y):
+        self.input.put(x)
+        self.input.put(y)
+        self.is_idle = False
 
     def _input(self):
         # Read input, or -1
         try:
             return self.input.get(block=False)
         except Empty:
+            if not self.is_idle:
+                self.router.route_for_vm(self.vm_id, IDLE_SENTINEL)
+                log_debug(f"VM {self.vm_id} is going idle")
             self.is_idle = True
             return -1
 
@@ -46,14 +51,14 @@ class NicVM(VMThread):
         self.output.append(item)
         if len(self.output) == 3:
             # Received a complete output
-            addr, x, y = self.output
+            self.router.route_for_vm(self.vm_id, self.output)
             self.output = []
-            self.router.route(addr, x, y)
 
 
 class VMRouter:
     def __init__(self, mem, n):
-        self.result = queue.Queue()
+        self.stopped = False
+        self.queue = queue.Queue()
         self.last_x = None
         self.last_y = None
         self.last_y_sent = None
@@ -63,56 +68,50 @@ class VMRouter:
             vm.input.put(i)
             self.vms[i] = vm
 
-    def route(self, addr, x, y):
-        log_debug(f"Routing: {addr} => {x} {y}")
-        vm = self.vms.get(addr)
-        if vm is not None:
-            with vm.input_lock:
-                vm.input.put(x)
-                vm.input.put(y)
-                vm.is_idle = False
-        else:
-            if addr == 255:
-                self.result.put((x, y))
-            else:
-                log_error(f"route: bad address {addr}")
+    def route_for_vm(self, vm_id, data):
+        self.queue.put((vm_id, data))
 
     def run(self):
         for vm in self.vms.values():
             vm.start()
         log_debug("Waiting for result ...")
         while True:
-            try:
-                x, y = self.result.get(timeout=1)
-                log_info(f"NAT: received {x}, {y}")
-                if self.last_y is None:
-                    log_always("Part 1")
-                    log_always(y)
-                self.last_x = x
-                self.last_y = y
-            except Empty:
-                # Check if all VMs are idle
-                if all([vm.is_idle for vm in self.vms.values()]):
-                    log_info("VMs are idle")
-                    if self.last_x is not None:
-                        if self.last_y_sent == self.last_y:
-                            log_always("Part 2")
-                            log_always(self.last_y)
-                            break
-                        self.last_y_sent = self.last_y
-                        vm = self.vms[0]
-                        log_info(f"NAT: sending {self.last_x}, {self.last_y}")
-                        vm.input.put(self.last_x)
-                        vm.input.put(self.last_y)
-                        vm.is_idle = False
-                    else:
-                        log_error("Error: NAT has no stored data")
+            vm_id, data = self.queue.get()
+            if data is IDLE_SENTINEL:
+                if self.queue.empty():
+                    # C heck if all VMs are idle
+                    if all([vm.is_idle for vm in self.vms.values()]):
+                        log_info("VMs are idle")
+                        if self.last_x is not None:
+                            if self.last_y_sent == self.last_y:
+                                log_always("Part 2")
+                                log_always(self.last_y)
+                                break
+                            self.last_y_sent = self.last_y
+                            vm = self.vms[0]
+                            log_info(f"NAT: sending {self.last_x}, {self.last_y}")
+                            vm.send_packet(self.last_x, self.last_y)
+                        else:
+                            log_error("Error: NAT has no stored data")
+            else:
+                log_debug(f"Routing {vm_id} => {data}")
+                addr, x, y = data
+                vm = self.vms.get(addr)
+                if vm is not None:
+                    vm.send_packet(x, y)
+                elif addr == 255:
+                    # Packet to NAT
+                    log_info(f"NAT: received {x}, {y}")
+                    if self.last_y is None:
+                        log_always("Part 1")
+                        log_always(y)
+                    self.last_x = x
+                    self.last_y = y
+                else:
+                    log_error(f"route: bad address {addr}")
 
-                # if all()
-                pass
         # Stop all VMs
-        for vm in self.vms.values():
-            vm.stop()
+        self.stopped = True
         for vm in self.vms.values():
             vm.join()
 
