e882ba6a1f141799e068c88e456b24fede328499
David <dave.rodda@gmail.com>
2022-12-13 18:14:35 +1100

    2022 Challenge 13: Improved

--- aoc_2022/challenge_13.py
+++ aoc_2022/challenge_13.py
@@ -1,79 +1,71 @@
 #!/usr/bin/env python3
 
-from functools import cmp_to_key
+import ast
+import functools
 import itertools
 import sys
 import traceback
 from common.utils import *
 
 
-PART2_DIVIDERS = [
-    "[[2]]",
-    "[[6]]",
-]
+PART2_DIVIDERS = """
+[[2]]
+[[6]]
+""".strip().splitlines()
 
 
-def in_order(left, right):
-    result = None
+def compare(left, right):
+    """ Compare left to right
+        return -1 if left is smaller, 0 if the same, 1 if larger
+    """
+    # Integers: compare directly
     if isinstance(left, int) and isinstance(right, int):
-        if left != right:
-            result = left < right
-    else:
-        if isinstance(left, int):
-            left = [left]
-        if isinstance(right, int):
-            right = [right]
-
-        for _left, _right in zip(left, right):
-            _result = in_order(_left, _right)
-            if _result is not None:
-                result = _result
-                break
-        if result is None:
-            if len(left) != len(right):
-                result = len(left) < len(right)
-    return result
+        return int(left > right) - int(left < right)
+    # Convert any remaining integers to lists
+    if isinstance(left, int):
+        left = [left]
+    if isinstance(right, int):
+        right = [right]
+    # Compare matching elements
+    for _left, _right in zip(left, right):
+        _result = compare(_left, _right)
+        if _result != 0:
+            return _result
+    # Compare length of lists
+    return compare(len(left), len(right))
 
 
 def solve_part1(data):
     result = 0
-    for i, (_left, _right) in enumerate(data):
-        # Convert to Python objects
-        left = eval(_left)
-        right = eval(_right)
-        _result = in_order(left, right)
-        if _result:
+    for i, (left, right) in enumerate(data):
+        _result = compare(left, right)
+        if _result == -1:
             result += (i + 1)
     return result
 
 
 def solve_part2(data):
-    def comparator(x, y):
-        _result = in_order(x, y)
-        if _result is True:
-            return 1
-        elif _result is None:
-            return 0
-        else:
-            return -1
-
-    # Convert part 2 divisors to Python objects
-    divisors = [eval(item) for item in PART2_DIVIDERS]
-    # Merge all lines in data, convert to Python objects
-    data = [eval(item) for item in itertools.chain(*data)] + divisors
-    # Sort
-    data = sorted(data, key=cmp_to_key(comparator), reverse=True)
-    # Find index of divisors
+    # Evaluate part 2 dividers to Python objects
+    dividers = [ast.literal_eval(item) for item in PART2_DIVIDERS]
+    # Merge all lines in data
+    data = list(itertools.chain(*data)) + dividers
+    # Sort using custom comparator
+    data = sorted(data, key=functools.cmp_to_key(compare))
+    # Find index of dividers
     result = 1
     for i, v in enumerate(data):
-        if v in divisors:
+        if v in dividers:
             result *= (i + 1)
     return result
 
 
 def main():
     args = parse_args()
-    data = list(read_multilines(data_file_path_main(test=args.test)))
+    data = [
+        # Evaluate to Python objects
+        list(map(ast.literal_eval, items))
+        for items in read_multilines(data_file_path_main(test=args.test))
+    ]
 
     log.always("Part 1:")
     result = solve_part1(data)
