430c1d4e44ac58588b566b37683c3f3807d1e8c0
David <dave.rodda@gmail.com>
2024-02-10 17:20:20 +1100

    2023 Day 24

--- aoc_2023/challenge_24.py
+++ aoc_2023/challenge_24.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+
 import contextlib
 import dataclasses
 import math
 import sys
 import traceback
+from typing import TypeAlias
+
 from common.utils import *
+
+import sympy
 
 
 @dataclasses.dataclass
@@ -32,6 +37,9 @@ class Vector:
         return self.xy_m * x + self.xy_c
 
 
+Vectors: TypeAlias = list[Vector]
+
+
 def calculate_intersection(v1: Vector, v2: Vector):
     if math.isclose(v1.xy_m, v2.xy_m):
         raise ValueError(f"Vectors are parallel")
@@ -42,7 +50,7 @@ def calculate_intersection(v1: Vector, v2: Vector):
     return x, y
 
 
-def solve(vectors, min_value, max_value):
+def solve_part1(vectors: Vectors, min_value, max_value):
     result = 0
     for i, v1 in enumerate(vectors):
         for j in range(i + 1, len(vectors)):
@@ -55,6 +63,35 @@ def solve(vectors, min_value, max_value):
     return result
 
 
+def solve_part2(vectors: Vectors):
+    # Create symbols for thrown stone vector
+    x0 = sympy.Symbol('x')
+    y0 = sympy.Symbol('y')
+    z0 = sympy.Symbol('z')
+    vx = sympy.Symbol('vx')
+    vy = sympy.Symbol('vy')
+    vz = sympy.Symbol('vz')
+    symbols = [x0, y0, z0, vx, vy, vz]
+    equations = []
+    for i, v in enumerate(vectors[:3]):
+        # Create equations time of intercept between stone and vector
+        t = sympy.Symbol(f"t{i}")
+        symbols.append(t)
+        equations.extend([
+            ((x0 + vx * t) - (v.x0 + v.vx * t)),
+            ((y0 + vy * t) - (v.y0 + v.vy * t)),
+            ((z0 + vz * t) - (v.z0 + v.vz * t)),
+        ])
+    solutions = sympy.solve_poly_system(equations, symbols)
+    if len(solutions) == 0:
+        log.error(f"No solutions found")
+        return None
+    solution = solutions[0][:6]
+    log.info(f"Solution: {solution}")
+    val_x0, val_y0, val_z0, *_ = solution
+    return val_x0 + val_y0 + val_z0
+
+
 def main():
     args = parse_args()
     lines = read_lines(data_file_path_main(test=args.test), to_list=True)
@@ -64,12 +101,12 @@ def main():
     ]
 
     log.always("Part 1:")
-    result = solve(vectors, *([7, 27] if args.test else [200000000000000, 400000000000000]))
+    result = solve_part1(vectors, *([7, 27] if args.test else [200000000000000, 400000000000000]))
     log.always(result)
 
-    # log.always("Part 2:")
-    # result = solve(vectors)
-    # log.always(result)
+    log.always("Part 2:")
+    result = solve_part2(vectors)
+    log.always(result)
 
 
 if __name__ == "__main__":
